0cfc3f00fc10e7b02bae2b3768b86aba9814ace5
Prajeesha <praji22@gmail.com>
2023-06-26 17:47:55 +0530

    Update app.js
    Inventory sub module backend

--- app.js
+++ app.js
@@ -15,8 +15,55 @@ app.use(Cors())
 
 Mongoose.connect("mongodb+srv://webprojectspraji:2020web@cluster0.ojqbnfv.mongodb.net/ItemDB?retryWrites=true&w=majority",{useNewUrlParser:true})
 
-app.get("/",(req,res)=>{
-    res.send("app working")
+app.post('/additems',async(req,res)=>{
+    let data = new ItemModel(req.body)
+    console.log(data)
+    await data.save()
+    res.json({"status":"success","data":data})
+})
+
+app.get('/displayitems',async(req,res)=>{
+    try {
+        const result = await ItemModel.find();
+        res.json(result);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+      }
+})
+
+app.post('/additemgroups',async(req,res)=>{
+    let data = new ItemgroupModel(req.body)
+    console.log(data)
+    await data.save()
+    res.json({"status":"success","data":data})
+})
+
+app.get('/viewitemgroups',async(req,res)=>{
+    try {
+        const result = await ItemgroupModel.find();
+        res.json(result);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+      }
+})
+
+app.post('/addinventoryadjust',async(req,res)=>{
+    let data= new InventoryModel(req.body)
+    console.log(data)
+    await data.save()
+    res.json({"status":"success","data":data})
+})
+
+app.get('/viewinventory',async(req,res)=>{
+    try {
+        const result = await InventoryModel.find();
+        res.json(result);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+      }
 })
 
 app.listen(3002,()=>{
