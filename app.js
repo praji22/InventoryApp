d69f8cca6ff18a2cec23d02c792a3e2a621134ce
Prajeesha <praji22@gmail.com>
2023-06-26 17:43:54 +0530

    MongoDB atlas Connections are made
    Created models and database collections

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const Mongoose = require('mongoose')
 const Cors =require('cors')
 const BodyParser =require('body-parser')
 
+const {ItemModel } = require('./Models/items')
+const{ItemgroupModel} = require('./Models/itemgroups')
+const { InventoryModel } = require('./Models/inventoryadjust')
+const{CustomerModel} = require('./Models/customers')
+
 let app = Express()
 app.use(BodyParser.urlencoded({extended:true}))
 app.use(BodyParser.json())
 app.use(Cors())
+
+Mongoose.connect("mongodb+srv://webprojectspraji:2020web@cluster0.ojqbnfv.mongodb.net/ItemDB?retryWrites=true&w=majority",{useNewUrlParser:true})
 
 app.get("/",(req,res)=>{
     res.send("app working")
